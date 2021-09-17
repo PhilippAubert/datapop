ac148216479c1f366ef279aeb7dcfec4d92acda9
philipp aubert <p.b.m.aubert@gmail.com>
2021-09-17 10:51:48 +0200

    Add basic input and textarea

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,17 @@ export default function About() {
         <div className="About-List__Headline">
           <h1>WRITE YOUR POST HERE </h1>
         </div>
+        <form className="Input-Form">
+          <label className="Input-Form_Label" for="title">
+            ENTER TITLE
+          </label>
+          <input type="text" key="id" id="text_id" placeholder="enter title" />
+          <label className="Input-Form_Label" for="post">
+            ENTER POST
+          </label>
+          <textarea type="text" id="id" placeholder="write post here" />
+          <button type="submit">send post</button>
+        </form>
       </div>
     </div>
   );
