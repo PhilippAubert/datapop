3e2adb7b83e79e13abb20c6fa9e6b2cc0ddaafab
philipp aubert <p.b.m.aubert@gmail.com>
2021-09-17 09:10:14 +0200

    Add dummy-posts and very basic styling

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -5,8 +5,39 @@ export default function Users() {
   return (
     <div className="Main">
       <div className="Users-List">
-        <div className="Users-List__Headline">
-          <h1>ALL POSTS SO FAR</h1>
+        <div className="Posts">
+          <h2 className="Posts_Headline">Placeholder Entry</h2>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <div className="Buttons">
+            <button className="Button">Delete</button>
+            <button className="Button">Edit</button>
+          </div>
+        </div>
+      </div>
+      <div className="Users-List">
+        <div className="Posts">
+          <h2 className="Posts_Headline">Placeholder Entry</h2>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <div className="Buttons">
+            <button className="Button">Delete</button>
+            <button className="Button">Edit</button>
+          </div>
+        </div>
+      </div>
+      <div className="Users-List">
+        <div className="Posts">
+          <h2 className="Posts_Headline">Placeholder Entry</h2>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <p className="Posts_Text">Blind text for dummy representation</p>
+          <div className="Buttons">
+            <button className="Button">Delete</button>
+            <button className="Button">Edit</button>
+          </div>
         </div>
       </div>
     </div>
