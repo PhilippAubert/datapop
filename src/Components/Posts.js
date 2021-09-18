2e3838ba0aeebb5c89bd96b590f6553eeccecbb4
philipp aubert <p.b.m.aubert@gmail.com>
2021-09-18 19:29:17 +0200

    Leaving the delete function in child-component for now

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -2,6 +2,17 @@ import React from "react";
 import "./CSS/Posts.css";
 
 export default function Posts({ post }) {
+  function handleDelete() {
+    const options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(post),
+    };
+
+    fetch(`http://localhost:3005/spark/${post._id}`, options).then(() =>
+      console.log(`Post  deleted`)
+    );
+  }
   return (
     <div className="Posts">
       <h2 className="Posts_Headline">{post.title}</h2>
@@ -9,7 +20,9 @@ export default function Posts({ post }) {
         <p className="Posts_Text">{post.body}</p>
       </div>
       <div className="Buttons">
-        <button className="Button">Delete</button>
+        <button onClick={handleDelete} className="Button">
+          Delete
+        </button>
         <button className="Button">Edit</button>
       </div>
     </div>
